88428ee6cb8510008d4e3745cf0bd3109af0dbf4
Adrien Guinet <aguinet@quarkslab.com>
2017-01-02 18:34:04 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="arybo",
-    version="0.2.0",
+    version="0.4.0",
     author="Adrien Guinet",
     author_email="aguinet@quarkslab.com",
     description="Manipulation, canonicalization and identification of mixed boolean-arithmetic symbolic expressions",
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "networkx",
         "ipython",
-        "pytanque >= 0.1",
+        "pytanque >= 0.4",
         "six"
     ],
     classifiers=[
